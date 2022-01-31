b4e035dbce3136286fb2b35f3246fdebcbaf1419
Shmuli Margulies <smargulies@expediagroup.com>
2022-01-31 23:17:43 +0000

    added endpoint to delete playlists currently it just lists them

--- routes/index.js
+++ routes/index.js
@@ -14,18 +14,59 @@ router.get('/login', function (req, res, next) {
   res.redirect(authorizeURL)
 });
 
+router.get("/delete_playlists", async (req, res, next) => {
+  const errHandler = err => {
+    console.log('Something went wrong!', err);
+    res.json(err)
+  }
+
+  const userData = await spotifyApi.getMe()
+  const user = userData.body
+  let pListsToDelete = []
+  let totalPlaylists = 0
+  let offset = 0
+
+  const timer = setInterval(() => {
+    spotifyApi.getUserPlaylists(user.id, {
+      limit: 50,
+      offset: offset
+    }).then(resp => {
+      const {total, items} = resp.body
+      totalPlaylists = total
+      offset += 50
+
+      for (const itemsKey in items) {
+        if (items[itemsKey].name === 'Pure Go' || items[itemsKey].tracks.total === 0) {
+          pListsToDelete.push({
+            id: items[itemsKey].id,
+            name: items[itemsKey].name,
+            tracks: items[itemsKey].tracks.total
+          })
+        }
+      }
+
+      if (offset > 100) {
+        clearInterval(timer)
+        console.log("interval cleared")
+        res.json(pListsToDelete)
+      }
+    }).catch(errHandler)
+  }, 350)
+})
+
+function deletePlaylist(playlistsToDelete) {
+  for (const key in playlistsToDelete) {
+    spotifyApi.unfollowPlaylist(playlistsToDelete[key].id)
+  }
+}
+
 router.get('/callback', function (req, res, next) {
   // your application requests refresh and access tokens
-  // after checking the state parameter
 
   const code = req.query.code || null;
 
   spotifyApi.authorizationCodeGrant(code)
     .then(data => {
-        console.log('The token expires in ' + data.body['expires_in']);
-        console.log('The access token is ' + data.body['access_token']);
-        console.log('The refresh token is ' + data.body['refresh_token']);
-
         // Set the access token on the API object to use it in later calls
         spotifyApi.setAccessToken(data.body['access_token']);
         spotifyApi.setRefreshToken(data.body['refresh_token']);
@@ -34,11 +75,7 @@ router.get('/callback', function (req, res, next) {
         console.log('Something went wrong!', err);
       }
     ).then(() => {
-      spotifyApi.getMe().then(function (data) {
-        console.log('Some information about the authenticated user', data.body);
-      }, function (err) {
-        console.log('Something went wrong!', err);
-      })
+      res.redirect("/delete_playlists")
     }
   )
 });
